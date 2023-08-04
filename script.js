86eb14a3b4d8017f738c4f2f11129a53b8070c21
arif3ana <ariftriana28@gmail.com>
2023-08-04 09:38:37 +0700

    feat: restore input empty and event button

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let todo = [];
 const data = JSON.parse(localStorage.getItem("TODO"));
 if (data !== null) {
   for (const todoList of data) {
-    console.log(todoList);
+    todo.push(todoList);
   }
 }
 
@@ -13,12 +13,25 @@ const ul = document.querySelector(".todo");
 
 btn.addEventListener("click", () => {
   addToLocalStorage();
+  addContent();
+  input.value = "";
+  btn.classList.add("clicked");
+  setTimeout(() => {
+    btn.classList.remove("clicked");
+  }, 300);
 });
 
 const addToLocalStorage = () => {
+  if (input.value == "") {
+    return setTimeout(() => alert("kamu belum menuliskan task!!"), 300);
+  }
   todo.push(input.value);
   localStorage.setItem("TODO", JSON.stringify(todo));
 };
+
+window.addEventListener("load", function () {
+  addContent();
+});
 
 // membuat elemnt html untuk menampilkan todo
 const addContent = () => {
